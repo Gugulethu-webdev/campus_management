eada990d7f7c32929e7f7c32d9535342218cf0c2
Gugulethu-webdev <mtshaliamo@gmail.com>
2020-08-21 19:46:02 +0200

    Updates

---
+++ precache-manifest.26738a3b7c56aa64ffc4aa39d223d3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73f960dd236d247e84c417929e7a99b",
+    "url": "/campus_management/index.html"
+  },
+  {
+    "revision": "def6ed1399334c42aed2",
+    "url": "/campus_management/static/css/main.908d2985.chunk.css"
+  },
+  {
+    "revision": "a799bc7a6b928b5d3ee8",
+    "url": "/campus_management/static/js/2.d92932c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/campus_management/static/js/2.d92932c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def6ed1399334c42aed2",
+    "url": "/campus_management/static/js/main.6670e153.chunk.js"
+  },
+  {
+    "revision": "70a9cffe01002c4bf78b",
+    "url": "/campus_management/static/js/runtime-main.56999ec3.js"
+  }
+]);
